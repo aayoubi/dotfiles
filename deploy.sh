dfd4b124b53262cff02c9cf6d83faaaa6c0fe4f6
Ali AYOUBI <ali.ayoubi@murex.com>
2016-08-05 12:02:32 +0000

    Add Vundle as a submodule; deploy.sh now deploys the dotfiles to the HOME directory

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
+git submodule init
+git submodule update
 
+ln -s ./bashrc ~/.bashrc
+ln -s ./vimrc ~/.vimrc
+ln -s ./gitconfig ~/.gitconfig
+ln -s ./zshrc ~/.zshrc
+ln -s ./tmux.conf ~/.tmux.conf
+
+git submodule foreach git pull origin master --recurse-submodules
 
